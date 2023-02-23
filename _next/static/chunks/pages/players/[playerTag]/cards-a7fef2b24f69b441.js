e4b022682264884b3e8e4e876f6c5ce5918190ce
viphakorn <viphakone585@gmail.com>
2023-02-23 13:01:41 +0700

    init

---
+++ _next/static/chunks/pages/players/[playerTag]/cards-a7fef2b24f69b441.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[801],{2366:function(n,e,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/players/[playerTag]/cards",function(){return u(9662)}])},9662:function(n,e,u){"use strict";u.r(e);var r=u(5893);let t=()=>(0,r.jsx)("div",{children:"Enter"});e.default=t}},function(n){n.O(0,[774,888,179],function(){return n(n.s=2366)}),_N_E=n.O()}]);
